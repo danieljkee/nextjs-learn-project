f72369506ca2127bcd0b7836fce1674b353059f0
danieljkee <danieljkee7@gmail.com>
2024-06-11 20:47:29 +0700

    modified:   app/layout.tsx modified:   package-lock.json modified:   package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@
 import '@/app/ui/global.css';
 // добавление шрифта глобально!
 import { inter } from './ui/fonts';
+// для отслеживания метрик производительности приложения
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function RootLayout({
   children,
@@ -10,7 +12,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
